6d7f0349392b620b30e57a42dbf278b08e4a7f4c
Emanuele <egalli64@gmail.com>
2024-11-15 14:18:54 +0100

    fix jees dependency removed

--- src/main/java/com/example/cfs1/servlet/Login.java
+++ src/main/java/com/example/cfs1/servlet/Login.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.example.jees.m1.s3.MultipleParamReader;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,35 +13,35 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
-	private static final Logger log = LogManager.getLogger(Login.class);
-	private static final long serialVersionUID = 1L;
+    private static final Logger log = LogManager.getLogger(Login.class);
+    private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-		String email = request.getParameter("e-mail");
-		String password = request.getParameter("password");
-		
-		if (email == null || email.isEmpty()) {
-			log.warn("No e-mail passed as parameter!");
-			request.getRequestDispatcher("error.html").forward(request, response); // Inoltra alla pagina di errore
+        String email = request.getParameter("e-mail");
+        String password = request.getParameter("password");
+
+        if (email == null || email.isEmpty()) {
+            log.warn("No e-mail passed as parameter!");
+            request.getRequestDispatcher("error.html").forward(request, response); // Inoltra alla pagina di errore
             return;
-		} else {
-			log.trace("%s as e-mail", email);
-		}
+        } else {
+            log.trace("%s as e-mail", email);
+        }
 
-		if (password == null || password.isEmpty()) {
-		    log.warn("No password passed as parameter or it is empty!");
-		    request.getRequestDispatcher("error.html").forward(request, response); // Inoltra alla pagina di errore
+        if (password == null || password.isEmpty()) {
+            log.warn("No password passed as parameter or it is empty!");
+            request.getRequestDispatcher("error.html").forward(request, response); // Inoltra alla pagina di errore
             return;
-		}	
+        }
 
-		if ("admin@google.it".equals(email) && "password123".equals(password)) {
-			response.sendRedirect("Homepage.html");
-		} else {
-		    request.setAttribute("errorMessage", "Password errata. Riprova.");
-		 
-		} 
-		
-	}
+        if ("admin@google.it".equals(email) && "password123".equals(password)) {
+            response.sendRedirect("Homepage.html");
+        } else {
+            request.setAttribute("errorMessage", "Password errata. Riprova.");
+
+        }
+
+    }
 }
--- src/main/java/com/example/cfs1/servlet/Register.java
+++ src/main/java/com/example/cfs1/servlet/Register.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.example.jees.m1.s3.MultipleParamReader;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,38 +13,38 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/registrati")
 public class Register extends HttpServlet {
-	private static final Logger log = LogManager.getLogger(Register.class);
-	private static final long serialVersionUID = 1L;
+    private static final Logger log = LogManager.getLogger(Register.class);
+    private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String email = request.getParameter("Email");
-		String password = request.getParameter("Password");
-		String confermaPassword = request.getParameter("Conferma Password");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("Email");
+        String password = request.getParameter("Password");
+        String confermaPassword = request.getParameter("Conferma Password");
 
-		if (email == null || email.isEmpty()) {
-			log.warn("No e-mail passed as parameter!");
-			request.getRequestDispatcher("error.html").forward(request, response);
-			return;
-		} else {
-			log.trace("%s as e-mail", email);
-		}
+        if (email == null || email.isEmpty()) {
+            log.warn("No e-mail passed as parameter!");
+            request.getRequestDispatcher("error.html").forward(request, response);
+            return;
+        } else {
+            log.trace("%s as e-mail", email);
+        }
 
-		if (password == null || password.isEmpty()) {
-			log.warn("No password passed as parameter or it is empty!");
-			request.getRequestDispatcher("password_mismatch.html").forward(request, response);
-			return;
-		}
+        if (password == null || password.isEmpty()) {
+            log.warn("No password passed as parameter or it is empty!");
+            request.getRequestDispatcher("password_mismatch.html").forward(request, response);
+            return;
+        }
 
-		if (!password.equals(confermaPassword)) {
-			response.getWriter().println("Le password non corrispondono.");
-			response.sendRedirect("password_mismatch.html");
-			return;
-		}
-		log.info("User registered successfully with %s as e-mail", email);
+        if (!password.equals(confermaPassword)) {
+            response.getWriter().println("Le password non corrispondono.");
+            response.sendRedirect("password_mismatch.html");
+            return;
+        }
+        log.info("User registered successfully with %s as e-mail", email);
 
-		request.getRequestDispatcher("registration_success.html").forward(request, response);
+        request.getRequestDispatcher("registration_success.html").forward(request, response);
 
-	}
+    }
 
 }
