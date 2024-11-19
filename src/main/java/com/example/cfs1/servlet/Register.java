6ee53346f479642992047b6b53d95792993fe98f
Antonella Castria <antonellacastria1@gmail.com>
2024-11-19 11:11:05 +0100

    fixed

--- src/main/java/com/example/cfs1/servlet/Register.java
+++ src/main/java/com/example/cfs1/servlet/Register.java
@@ -51,6 +51,20 @@ public class Register extends HttpServlet {
 			request.getRequestDispatcher("Register.jsp").forward(request, response);
 			return;
 		}
+		try {
+			boolean success = userDao.insertUser(email, password);
+			if (success) {
+				 log.info("User registered successfully with email: {}", email);
+				request.getRequestDispatcher("AreaPersonale.html").forward(request, response);
+
+			} else {
+				request.setAttribute("error", "Errore. Riprova più tardi");
+				request.getRequestDispatcher("Register.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			log.error("Error during user registration", e);
+			throw new ServletException("Errore durante la registrazione dell'utente", e);
+		}
 
 		try (EmailDao userDao = new EmailDao()) {
 			if (userDao.isEmailExists(email)) {
@@ -67,23 +81,9 @@ public class Register extends HttpServlet {
 			log.error("Error during user registration", e);
 			throw new ServletException("Errore durante la registrazione dell'utente", e);
 		}
-		boolean success;
-		try {
-			success = userDao.insertUser(email, password);
-			if (success) {
-				request.getRequestDispatcher("AreaPersonale.html").forward(request, response);
-				return;
-
-			} else {
-				request.setAttribute("error", "Errore. Riprova più tardi");
-				request.getRequestDispatcher("Register.jsp").forward(request, response);
-			}
-		} catch (Exception e) {
-			log.error("Error during user registration", e);
-			throw new ServletException("Errore durante la registrazione dell'utente", e);
-		}
-		log.info("User registered successfully with %s as e-mail", email);
-		return;
+		
+		
+	
 	}
 
 }
